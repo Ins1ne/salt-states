1e0d3bb17d52993f833ccbdf6f9d913ebee31cb3
Aider Ibragimov <aider.ibragimov@gmail.com>
2013-04-18 16:06:37 +0300

    >_<

--- salt/_runners/carbonmon.py
+++ salt/_runners/carbonmon.py
@@ -1,5 +1,4 @@
 import salt.client
-import sys
 
 
 def pollpush():
@@ -7,8 +6,8 @@ def pollpush():
     Run the monitoring command and return to carbon
     '''
     client = salt.client.LocalClient(__opts__['conf_file'])
-    cmds = ('virt.vm_diskstats', 'virt.vm_netstats', 'virt.vm_cputime')
-    nodes = 'chani,harkonnen,fremen'
+    cmds = ('ps.physical_memory_usage', 'ps.cpu_percent')
+    nodes = '*'
 
     for cmd in cmds:
-        jid = client.cmd_async(nodes, cmd, expr_form='list', ret='carbon', timeout=__opts__['timeout'])
+        jid = client.cmd_async(nodes, cmd, ret='carbon', timeout=__opts__['timeout'])
